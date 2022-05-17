1006b41d211edad4f9d4ba425ed023c4e1196e1f
m4ks0n <limos.00@mail.ru>
2022-05-17 20:10:36 +0300

    plot figure was added

--- ui.py
+++ ui.py
@@ -1,13 +1,64 @@
 from tkinter import *
 import tkinter.ttk as ttk
 
+import matplotlib
+
+matplotlib.use('TkAgg')
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+
 def create_ui():
     root = Tk()
     root.geometry('1200x720')
     root.title("Graph builder")
+    #todo make root not resizeable
 
-    combobox_canvas = Canvas(root, width=230, height=100, bg='white', highlightthickness=0)
+    combobox_canvas = Canvas(root, width=150, height=75, bg='white', highlightthickness=0)
     combobox_canvas.place(x=30, y = 70)
+
+    f = Figure(figsize=(5, 4), dpi=100)
+
+    graph = nx.Graph()
+
+
+
+
+
+    cities = {'A': (0, 20),
+              'B': (15, 24),
+              'C': (16, 41),
+              'D': (10, 40)}
+
+
+
+    def my_add_edge(f_item, s_item, graph=None):
+        graph.add_edge(f_item, s_item)
+        graph.add_edge(s_item, f_item)
+
+    my_add_edge('A', 'B', graph=graph)
+    my_add_edge('B', 'C', graph=graph)
+    my_add_edge('B', 'D', graph=graph)
+
+    nx.draw_circular(graph,
+                     node_color='red',
+                     node_size=1000,
+                     with_labels=True)
+
+
+
+
+
+
+
+    canvs = FigureCanvasTkAgg(f, root)
+    canvs.get_tk_widget().place(x=360, y=100)
+
+    plt.show()
 
     graph_type = ttk.Combobox(combobox_canvas)
     graph_type.place(x = 0, y = 0)
